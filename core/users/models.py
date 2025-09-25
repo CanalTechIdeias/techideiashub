97ab53766d0f1be4967619d0414284a6c8aff922
PedroEduardoSS <pedroeduardossousa@gmail.com>
2025-09-25 19:51:27 -0300

    Add birth date at profile

--- core/users/models.py
+++ core/users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.text import slugify
+from datetime import date
 
 
 # Create your models here.
@@ -13,6 +14,8 @@ class Profile(models.Model):
         on_delete=models.CASCADE
     )
     bio = models.TextField(max_length=500, blank=True)
+    birth_date = models.DateField(default=date.today, null=False, blank=False)
+    birth_date_edited = models.BooleanField(default=False)
     def user_profile_image_path(instance, filename):
         # File will be uploaded to MEDIA_ROOT/profile_images/<slug>/<filename>
         return f'profile_images/{instance.slug}/{filename}'
@@ -31,6 +34,12 @@ class Profile(models.Model):
                 slug = f"{base_slug}-{num}"
                 num += 1
             self.slug = slug
+        if self.pk is not None:
+            old = Profile.objects.get(pk=self.pk)
+            if old.birth_date != self.birth_date and old.birth_date_edited:
+                self.birth_date = old.birth_date
+            elif old.birth_date != self.birth_date:
+                self.birth_date_edited = True
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -40,5 +49,5 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        user_profile = Profile(user=instance)
+        user_profile = Profile(user=instance, birth_date=date.today())
         user_profile.save()
